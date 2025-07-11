b1cfe316b1bca5dc0a588d9c61f146039f5ed949
Robert Mrobo <r.mrobo@yahoo.com>
2025-07-11 03:46:06 +0200

    Added `flutter_native_splash` package and implemented platform-specific splash screens with customizable light and dark themes. Updated project configurations for proper integration.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:live_match_blogger/features/timer/pages/timer_page.dart';
 import 'package:live_match_blogger/features/timer/utils/theme.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(ProviderScope(child: const MyApp()));
 }
 
